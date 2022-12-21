bf111cc0e78a65ebd722152568fd9d7d5a9a06a6
Julia-Kalyukh <kalyuh.julia@gmail.com>
2022-12-21 20:18:00 +0300

    Добавлены категории

---
+++ src/js/tags.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+  let tagsContainer = document.querySelectorAll('.js-tags-container');
+
+  tagsContainer.forEach(item => {
+
+    let btnMore = item.querySelector('.js-tag-more'),
+      btnClose = item.querySelector('.js-tag-close'),
+      tagsNone = item.querySelectorAll('.js-tag-none');
+    
+    btnMore.addEventListener('click', function () {
+      tagsNone.forEach(tag => {
+        tag.style.cssText = 'display: inline-block;';
+      });
+      this.style.cssText = "display: none";
+    })
+  
+    btnClose.addEventListener('click', function () {
+      tagsNone.forEach(tag => {
+        tag.style.cssText = 'display: none;';
+      });
+      btnMore.style.cssText = "display: inline-block";
+    })
+  })
+});
